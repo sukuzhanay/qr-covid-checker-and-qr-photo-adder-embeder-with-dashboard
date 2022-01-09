08f0fcd9a27edd8347159a3cea511ffbbce2cef1
Lian Salmerón López <lembajosa@gmail.com>
2022-01-09 11:06:09 +0100

    Cálculo del tiempo medio desde la última vacuna

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import socket
+import sys
+import os
+import pandas
 import pyrebase as pb
 import streamlit as st
-import pandas as pd
-import plotly.graph_objects as go
+import datetime as dt
+from datetime import date
+from datetime import timedelta
+import plotly.graph_objects as plot
 
-# ¡¡¡API DE PRUEBA - base de datos falsa!!!
+# Base de datos de PRUEBA
 firebaseConfig = {
     "apiKey": "AIzaSyBsyBZDG0BF6F9BAi95G_bW_wVP3xt7_sc",
     "authDomain": "pcd-prueba.firebaseapp.com",
@@ -12,50 +18,106 @@ firebaseConfig = {
     "storageBucket": "pcd-prueba.appspot.com",
     "messagingSenderId": "547368512451",
     "appId": "1:547368512451:web:e7e22f6f578c608651ab76"
-}
+};
 
 firebase = pb.initialize_app(firebaseConfig)
 sign_in_up = firebase.auth()
+dd_bb = firebase.database()
 # Login de prueba
-mail = "mtrgomez00@gmail.com"
-passw = "RandomPass123"
-user = sign_in_up.sign_in_with_email_and_password(mail, passw)
-db = firebase.database()
+mail = "lembajosa@gmail.com"
+password = "patata123"
+user = sign_in_up.sign_in_with_email_and_password(mail, password)
 
-all_users = db.child("users").get()
+identificador = (input("Introduzca el id: "))
 
-spikevax = 0
-comirnaty = 0
-covishield = 0
-janssen = 0
+all_users = dd_bb.child("users").get()
+# Se obtiene la fecha de vacunacion del usuario
+ult_vacuna = dd_bb.child("users/" + identificador + "/fecha_vacunacion").get()
 
-print(all_users.val)
 
-# Contador de tipo de vacuna
-for users in all_users.each():
-    if "Spikevax" in str(users.val()):
-        spikevax += 1
-    elif "Comirnaty" in str(users.val()):
-        comirnaty += 1
-    elif "Covishield" in str(users.val()):
-        covishield += 1
+# Funcion que calcula el rango de dias entre una fecha y otra
+def date_range(start, end):
+    delta = end - start
+    days = [start + timedelta(days=i) for i in range(delta.days + 1)]
+    return delta.days
+
+
+x = []
+# Se realiza un split de la fecha recuperada de la base de datos
+x = str(ult_vacuna.val()).split("-")
+# Se guarda en un array
+anio_vacuna = int(x[0])
+mes_vacuna = int(x[1])
+dia_vacuna = int(x[2])
+
+# Se pasa a tipo Date
+tiempo_ult_vacuna = dt.date(anio_vacuna, mes_vacuna, dia_vacuna)
+# Se obtiene la fecha actual
+tiempo_actual = date.today()
+anio = tiempo_actual.year
+mes = tiempo_actual.month
+dia = tiempo_actual.day
+# Se guardan los dias entre las dos fechas
+dias_pasado = date_range(tiempo_ult_vacuna, tiempo_actual)
+
+meses_pasados = int(dias_pasado / 30)
+anios = int(meses_pasados / 12)
+print("Tiempo medio desde la ultima vacunacion: " + str(meses_pasados))
+
+if dias_pasado <= 30:
+    if dias_pasado == 1:
+        fig = plot.Figure(
+            data=[plot.Table(header=dict(values=['Tiempo medio desde la ultima vacunacion'], line_color='darkslategray',
+                                         fill_color='lightskyblue'),
+                             cells=dict(values=[[str(dias_pasado) + " dia "]]))
+                  ])
+
+
     else:
-        janssen += 1
+        fig = plot.Figure(
+            data=[plot.Table(header=dict(values=['Tiempo medio desde la ultima vacunacion'], line_color='darkslategray',
+                                         fill_color='lightskyblue'),
+                             cells=dict(values=[[str(dias_pasado) + " dias "]]))
+                  ])
 
-print("Spikevax " + str(spikevax) + "Comirnaty " + str(comirnaty) + "Covishield " + str(covishield) + "Janssen " + str(janssen))
 
-vacunas = ["Spikevax", "Comirnaty", "Covishield", "Janssen"]
-valores = [spikevax, comirnaty, covishield, janssen]
 
-# Se crea la grafica
-fig = go.Figure(
-    go.Pie(
-        labels=vacunas,
-        values=valores,
-        hoverinfo="label+percent",
-        textinfo="value"
-    ))
+else:
+    if meses_pasados <= 12:
+        if meses_pasados == 1:
+            fig = plot.Figure(
+                data=[plot.Table(
+                    header=dict(values=['Tiempo medio desde la ultima vacunacion'], line_color='darkslategray',
+                                fill_color='lightskyblue'),
+                    cells=dict(values=[[str(meses_pasados) + " mes"]]))
+                      ])
 
-# Grafica se pasa a Streamlit
-st.header("Vacunas")
-st.plotly_chart(fig)
+
+        else:
+            fig = plot.Figure(
+                data=[plot.Table(
+                    header=dict(values=['Tiempo medio desde la ultima vacunacion'], line_color='darkslategray',
+                                fill_color='lightskyblue'),
+                    cells=dict(values=[[str(meses_pasados) + " meses"]]))
+                      ])
+
+
+    else:
+        if anios == 1:
+            fig = plot.Figure(
+                data=[plot.Table(
+                    header=dict(values=['Tiempo medio desde la ultima vacunacion'], line_color='darkslategray',
+                                fill_color='lightskyblue'),
+                    cells=dict(values=[[str(anios) + " año"]]))
+                      ])
+
+
+        else:
+            fig = plot.Figure(
+                data=[plot.Table(
+                    header=dict(values=['Tiempo medio desde la ultima vacunacion'], line_color='darkslategray',
+                                fill_color='lightskyblue'),
+                    cells=dict(values=[[str(anios) + " años"]]))
+                      ])
+
+fig.show()
