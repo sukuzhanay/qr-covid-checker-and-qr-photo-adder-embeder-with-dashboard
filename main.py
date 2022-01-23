7a55cd52876cfa3844808b98361aa9a625d021b2
mohamedoussamafarhat <82153001+mohamedoussamafarhat@users.noreply.github.com>
2022-01-23 12:53:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,68 @@
 import pyrebase as pb
 import streamlit as st
 import pandas as pd
+
 firebaseConfig = {
-  "apiKey": "AIzaSyBsyBZDG0BF6F9BAi95G_bW_wVP3xt7_sc",
-  "authDomain": "pcd-prueba.firebaseapp.com",
-  "databaseURL": "https://pcd-prueba-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "pcd-prueba",
-  "storageBucket": "pcd-prueba.appspot.com",
-  "messagingSenderId": "547368512451",
-  "appId": "1:547368512451:web:e7e22f6f578c608651ab76"
+  "apiKey": "AIzaSyArmVriibDlBkIJ_3yURB1e3QprmU1z3hY",
+  "authDomain": "proyectofinalpcd-816d5.firebaseapp.com",
+  "databaseURL": "https://proyectofinalpcd-816d5-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "proyectofinalpcd-816d5",
+  "storageBucket": "proyectofinalpcd-816d5.appspot.com",
+  "messagingSenderId": "341285184166",
+  "appId": "1:341285184166:web:988dd980f771f7b150ca67",
+  "measurementId": "G-6DE3ERD9GD"
 }
 firebase = pb.initialize_app(firebaseConfig)
 db = firebase.database()
-all_users = db.child("users").get()
-
+all_users = db.child("Usuarios").get()
 pauta_completa=0
 pauta_incompleta=0
 alumnos=0
 
 print(all_users.val)
-
 # contar Alumnos con info adecuada
+
 for users in all_users.each():
-    if 'dosis' in str(users.val()):
-        
-        users_by_dosis = db.child("users/" + str(users.key()) + "/dosis").get()
-        alumnos+=1
-        print(str(users_by_dosis.key() + ': ') + str(users_by_dosis.val()))
-        if int(users_by_dosis.val()) > 1:
-            pauta_completa += 1
-        else:
-            pauta_incompleta += 1
-			
-def pautaCompleta():
-    pourcentaje1=(pauta_completa/3)*100
-    print("pourcentaje de alumnos que tengan pauta completa= ",pourcentaje1,"%")
-def pautaIncompleta():
-    pourcentaje2=(pauta_incompleta/alumnos)*100
-    print("pourcentaje de alumnos que tengan pauta completa= ",pourcentaje2,"%")
+        if 'Dosis' in str(users.val()):
+            
+            users_by_dosis = db.child("Usuarios/" + str(users.key()) + "/Datos_de_vacunacion/Dosis").get()
+            alumnos+=1
+            users_by_dosis_suministradas = db.child("Usuarios/" + str(users.key()) + "/Datos_de_vacunacion/Vacunas_suministradas").get()
 
-# percentaje pauta completa
-pautaCompleta()
+            print(str(users_by_dosis.key() + ': ') + str(users_by_dosis.val()))
+            if int(users_by_dosis_suministradas.val())==users_by_dosis.val():
+                pauta_completa += 1
+            else:
+                pauta_incompleta += 1
+# Alumnos con Pauta completa
+print("nombre de Alumnos con pauta completa: ",pauta_completa)
 
-# percentaje pauta incompleta
-pautaIncompleta()
+# Alumnos con pauta incompleta
+print("nombre de Alumnos con pauta incompleta: ",pauta_incompleta)
 
-
-import streamlit as st
+# Total de alumnos vacunados
+print("nombre de Alumnos vacunados: ",alumnos)
+# primer diagrama de pauta completa y incompleta
 import pandas as pd
 import altair as alt
+
+source = pd.DataFrame({"category": ["pauta completa", "pauta incompleta"], "value": [pauta_completa,pauta_incompleta]})
+
+alt.Chart(source).mark_arc(innerRadius=50).encode(
+    theta=alt.Theta(field="value", type="quantitative"),
+    color=alt.Color(field="category", type="nominal"),
+)
+
+# segundo diagrama
+import altair as alt
 import pandas as pd
-pourcentaje11=(pauta_incompleta/alumnos)*100
-pourcentaje22=(pauta_completa/alumnos)*100
+
 source = pd.DataFrame({
-    'a': ['una dosis', 'dos dosis'],
-    'b': [pourcentaje11, pourcentaje22]
+    'estado de vacunacion': [' pauta completa', ' pauta incompleta'],
+    'total de alumnos': [pauta_completa,pauta_incompleta ]
 })
 
 alt.Chart(source).mark_bar().encode(
-    x='a',
-    y='b'
+    x="estado de vacunacion",
+    y="total de alumnos"
 )
