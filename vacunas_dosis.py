b56cea56c55f5a56da2d3c6fec55932894e294a8
Carlos Moreno <mscrcarlos@gmail.com>
2022-01-23 10:44:26 +0100

    Adaptado a la base de datos correcta

--- vacunas_dosis.py
+++ vacunas_dosis.py
@@ -4,24 +4,25 @@ import plotly.graph_objects as go
 
 # ¡¡¡API DE PRUEBA - base de datos falsa!!!
 firebaseConfig = {
-    "apiKey": "AIzaSyBsyBZDG0BF6F9BAi95G_bW_wVP3xt7_sc",
-    "authDomain": "pcd-prueba.firebaseapp.com",
-    "databaseURL": "https://pcd-prueba-default-rtdb.europe-west1.firebasedatabase.app/",
-    "projectId": "pcd-prueba",
-    "storageBucket": "pcd-prueba.appspot.com",
-    "messagingSenderId": "547368512451",
-    "appId": "1:547368512451:web:e7e22f6f578c608651ab76"
+    "apiKey": "AIzaSyArmVriibDlBkIJ_3yURB1e3QprmU1z3hY",
+    "authDomain": "proyectofinalpcd-816d5.firebaseapp.com",
+    "databaseURL": "https://proyectofinalpcd-816d5-default-rtdb.europe-west1.firebasedatabase.app/",
+    "projectId": "proyectofinalpcd-816d5",
+    "storageBucket": "proyectofinalpcd-816d5.appspot.com",
+    "messagingSenderId": "341285184166",
+    "appId": "1:341285184166:web:988dd980f771f7b150ca67",
+    "measurementId": "G-6DE3ERD9GD"
 }
 
 firebase = pb.initialize_app(firebaseConfig)
 sign_in_up = firebase.auth()
 # Login de prueba
 mail = "mtrgomez00@gmail.com"
-passw = "RandomPass123"
+passw = "12345678"
 user = sign_in_up.sign_in_with_email_and_password(mail, passw)
 db = firebase.database()
 
-all_users = db.child("users").get()
+all_users = db.child("Usuarios").get()
 
 
 # =========== Contador de tipos de vacuna =================
@@ -95,5 +96,38 @@ def porcentaje_pauta():
     st.plotly_chart(fig)
 
 
+# Para la base de datos oficial
+def porcentaje_pauta_completa():
+    pauta_completa_bbdd = 0
+    pauta_incompleta_bbdd = 0
+
+    for users in all_users.each():
+        if 'Dosis' in str(users.val()):
+            users_by_dosis = db.child("Usuarios/" + str(users.key()) + "/Datos_de_vacunacion/Dosis").get()
+            users_by_dosis_suministradas = db.child("Usuarios/" + str(users.key()) + "/Datos_de_vacunacion/Vacunas_suministradas").get()
+
+            print(str(users_by_dosis.key() + ': ') + str(users_by_dosis.val()))
+            print(str(users_by_dosis_suministradas.key() + ': ') + str(users_by_dosis_suministradas.val()))
+            if str(users_by_dosis.val()) == str(users_by_dosis_suministradas.val()):
+                pauta_completa_bbdd += 1
+            else:
+                pauta_incompleta_bbdd += 1
+
+    print(pauta_completa_bbdd)
+    print(pauta_incompleta_bbdd)
+    # ====== PIE CHART - PORCENTAJE DOSIS =========
+
+    color_pauta = ['green', 'darkorange']
+    labels = ['Pauta completa', 'Pauta incompleta']
+    values = [pauta_completa_bbdd, pauta_incompleta_bbdd]
+
+    # Use `hole` to create a donut-like pie chart
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+    fig.update_traces(hoverinfo='label+value', textinfo='percent+label', textfont_size=20,
+                      marker=dict(colors=color_pauta, line=dict(color='#000000', width=2)))
+    st.header("Pauta de vacunación")
+    st.plotly_chart(fig)
+
+
 porcentaje_tipo_vacuna()
-porcentaje_pauta()
+porcentaje_pauta_completa()
