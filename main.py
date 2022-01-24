b29d657e4291f51e0b64e3daf5d81bb36cdbddb1
Lian Salmerón López <lembajosa@gmail.com>
2022-01-24 14:16:43 +0100

    Pauta media realizada

--- main.py
+++ main.py
@@ -263,9 +263,60 @@ def porcentaje_pauta_completa():
     st.plotly_chart(fig, use_container_width=True)
 
 
+def pauta_media():
+
+    all_users = dd_bb.child("Usuarios").get()
+    # contadores
+    pauta_completa = 0
+    pauta_incompleta = 0
+    alumnos = 0
+
+    # contar Alumnos con info adecuada
+
+    for users in all_users.each():
+        if 'Dosis' in str(users.val()):
+
+            users_by_dosis = dd_bb.child("Usuarios/" + str(users.key()) + "/Datos_de_vacunacion/Dosis").get()
+            alumnos += 1
+            users_by_dosis_suministradas = dd_bb.child(
+                "Usuarios/" + str(users.key()) + "/Datos_de_vacunacion/Vacunas_suministradas").get()
+
+            print(str(users_by_dosis.key() + ': ') + str(users_by_dosis.val()))
+            if int(users_by_dosis_suministradas.val()) == users_by_dosis.val():
+                pauta_completa += 1
+            else:
+                pauta_incompleta += 1
+    # Alumnos con Pauta completa
+    print("nombre de Alumnos con pauta completa: ", pauta_completa)
+
+    # Alumnos con pauta incompleta
+    print("nombre de Alumnos con pauta incompleta: ", pauta_incompleta)
+
+    # Total de alumnos vacunados
+    print("nombre de Alumnos vacunados: ", alumnos)
+
+    source = pd.DataFrame(
+        {"category": ["pauta completa", "pauta incompleta"], "value": [pauta_completa, pauta_incompleta]})
+
+    alt.Chart(source).mark_arc(innerRadius=50).encode(
+        theta=alt.Theta(field="value", type="quantitative"),
+        color=alt.Color(field="category", type="nominal"),
+    )
+
+    # segundo diagrama que presenta el n_alumnos de pauta completa, pauta incompleta y total de alumnos vacunados
+    fig = go.Figure(go.Bar(
+        x=[pauta_completa, pauta_incompleta],
+        y=['n_alumnos pauta completa', 'n_alumnos pauta incompleta'],
+        orientation='h'))
+
+    st.plotly_chart(fig)
+
+
 c = st.container()
+col1A, col2A = st.columns(2)
 with c:
-    tiempo_medio()
+    with col1A:
+        pauta_media()
 col1, col2, col3 = st.columns(3)
 with col1:
     porcentaje_tipo_vacuna()
